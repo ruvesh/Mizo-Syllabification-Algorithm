3c2f6a839d091e50c7b8d577efa3aefa371187a7
V. Ruvesh <49038280+ruvesh@users.noreply.github.com>
2019-08-03 12:52:28 +0530

    Add files via upload

---
+++ Mizo Language Syllabyfier/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Mizo_Language_Syllabyfier
+{
+    partial class MizoSyllabifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MizoSyllabifier));
+            this.enter_text = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.Label();
+            this.Syllable = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.load = new System.Windows.Forms.Button();
+            this.syllabify_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // enter_text
+            // 
+            this.enter_text.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.enter_text.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter_text.Location = new System.Drawing.Point(37, 28);
+            this.enter_text.Multiline = true;
+            this.enter_text.Name = "enter_text";
+            this.enter_text.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.enter_text.Size = new System.Drawing.Size(705, 222);
+            this.enter_text.TabIndex = 0;
+            this.enter_text.Text = "Mizo Text";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(162, 272);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 20);
+            this.result.TabIndex = 2;
+            // 
+            // Syllable
+            // 
+            this.Syllable.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Syllable.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Syllable.Location = new System.Drawing.Point(37, 265);
+            this.Syllable.Multiline = true;
+            this.Syllable.Name = "Syllable";
+            this.Syllable.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Syllable.Size = new System.Drawing.Size(705, 224);
+            this.Syllable.TabIndex = 4;
+            this.Syllable.Text = "Syllables";
+            // 
+            // save
+            // 
+            this.save.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.save.Location = new System.Drawing.Point(601, 519);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(141, 63);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save to File";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // load
+            // 
+            this.load.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.load.Location = new System.Drawing.Point(37, 519);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(132, 63);
+            this.load.TabIndex = 6;
+            this.load.Text = "Load from file";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // syllabify_button
+            // 
+            this.syllabify_button.Location = new System.Drawing.Point(310, 519);
+            this.syllabify_button.Name = "syllabify_button";
+            this.syllabify_button.Size = new System.Drawing.Size(132, 63);
+            this.syllabify_button.TabIndex = 7;
+            this.syllabify_button.Text = "Syllabify";
+            this.syllabify_button.UseVisualStyleBackColor = true;
+            this.syllabify_button.Click += new System.EventHandler(this.syllabify_button_Click);
+            // 
+            // MizoSyllabifier
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(773, 594);
+            this.Controls.Add(this.syllabify_button);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.Syllable);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.enter_text);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MizoSyllabifier";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mizo Language Syllabifier";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MizoSyllabifier_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox enter_text;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox Syllable;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button syllabify_button;
+    }
+}
+
---
+++ Mizo Language Syllabyfier/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Mizo_Language_Syllabyfier
+{
+    public partial class MizoSyllabifier : Form
+    {
+        private readonly SynchronizationContext sync;
+        private DateTime dt = DateTime.Now;
+
+        public MizoSyllabifier()
+        {
+            InitializeComponent();
+            sync = SynchronizationContext.Current;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            var openFile = new OpenFileDialog();
+            openFile.Title = "Browse";
+            openFile.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFile.FilterIndex = 1;
+            openFile.RestoreDirectory = true;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFile.FileName, FileMode.Open);
+                Byte[] b = new Byte[1024];
+                UTF8Encoding temp = new UTF8Encoding();
+                enter_text.Text = "";
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    enter_text.Text = enter_text.Text + temp.GetString(b);
+                }
+                fs.Close();
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string text = Syllable.Text.ToString();
+            var saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = @"E:\";
+            saveFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFile.FilterIndex = 1;
+            saveFile.RestoreDirectory = true;
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string path = Path.GetFullPath(saveFile.FileName);
+                File.AppendAllText(path, text);
+                MessageBox.Show("Text saved to file " + path, "Save Successful");
+            }
+            else
+            {
+                MessageBox.Show("Error in Saving File", "Save Error!");
+            }
+        }
+
+        private static bool isVowel(string word, int index)
+        {
+            bool ret = false;
+            switch (word[index])
+            {
+                case 'A':
+                case 'a':
+
+                case 'E':
+                case 'e':
+
+                case 'I':
+                case 'i':
+
+                case 'U':
+                case 'u':
+
+                    ret = true;
+                    break;
+
+                default:
+                    ret = false;
+                    break;
+            }
+
+            return ret;
+        }
+
+        private async void  syllabify_button_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => {
+                syllabifier_method();
+
+            } );
+           
+        }
+
+        private void syllabifier_method()
+        {
+            string enterredText = enter_text.Text.ToString();
+            List<string> words = enterredText.Replace(",", "").Replace(".", "").Replace(":", "").Split(' ').ToList();
+            int i = 0;
+            this.Invoke((MethodInvoker)delegate ()
+
+            {
+                Syllable.Text = "";
+
+                foreach (string word in words)
+                {
+                    for (i = 0; i < word.Length; i++)
+                    {
+                        Syllable.Text = Syllable.Text + word[i].ToString();
+                        
+                        if (i + 5 <= word.Length - 1)
+                        {
+                            if (isVowel(word, i))
+                            {
+                                if (word[i+1].Equals('\u0302') || word[i+1].Equals('\u030c'))
+                                {
+                                    if (word[i + 2].Equals('w') || word[i + 2].Equals('W'))
+                                    {
+                                        Syllable.Text += word[i + 1].ToString();
+                                        Syllable.Text += word[i + 2].ToString();
+                                        if(!isVowel(word, i+3))
+                                            Syllable.Text += word[i + 3].ToString();
+                                        Syllable.Text += ",";
+                                        i += 3;
+                                    }
+                                    else
+                                    {
+                                        Syllable.Text += word[i + 1].ToString();
+                                        Syllable.Text += word[i + 2].ToString();
+                                        Syllable.Text += ",";
+                                        i += 2;
+                                    }
+                                    
+                                }
+                            }
+                            else if((!isVowel(word, i) && isVowel(word, i + 1) && (word[i + 2].Equals('\u0302') || word[i + 2].Equals('\u030c'))))
+                            {
+                                if (word[i + 3].Equals('w') || word[i + 3].Equals('W'))
+                                {
+                                    if (word[i + 4].Equals('n') || word[i + 4].Equals('N'))
+                                    {
+                                        if (word[i + 5].Equals('G') || word[i + 5].Equals('g'))
+                                        {
+                                            Syllable.Text += word[i + 1].ToString();
+                                            Syllable.Text += word[i + 2].ToString();
+                                            Syllable.Text += word[i + 3].ToString();
+                                            Syllable.Text += word[i + 4].ToString();
+                                            Syllable.Text += word[i + 5].ToString();
+                                            Syllable.Text += ",";
+                                            i += 5;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Syllable.Text += word[i + 1].ToString();
+                                        Syllable.Text += word[i + 2].ToString();
+                                        Syllable.Text += word[i + 3].ToString();
+                                        if (!isVowel(word, i + 4))
+                                            Syllable.Text += word[i + 4].ToString();
+                                        Syllable.Text += ",";
+                                        i += 4;
+                                    }
+                                }
+                                else if (!isVowel(word, i + 3))
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += word[i + 2].ToString();
+                                    Syllable.Text += word[i + 3].ToString();
+                                    Syllable.Text += ",";
+                                    i += 3;
+                                }
+                                
+                            }
+                            else if ((!isVowel(word, i) && isVowel(word, i + 1) && !isVowel(word, i + 1 + 1)))
+                            {
+
+                                if (word[i + 1 + 1].Equals('W') || word[i + 1 + 1].Equals('w'))
+                                {
+                                    if (!isVowel(word, i + 1 + 1 + 1))
+                                    {
+                                        Syllable.Text += word[i + 1].ToString();
+                                        Syllable.Text += word[i + 2].ToString();
+                                        Syllable.Text += word[i + 3].ToString();
+                                        Syllable.Text += ",";
+                                        i += 3;
+                                    }
+                                }
+                                else if (word[i + 1 + 1 + 1].Equals('G') || word[i + 1 + 1 + 1].Equals('g'))
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += word[i + 2].ToString();
+                                    Syllable.Text += word[i + 3].ToString();
+                                    Syllable.Text += ",";
+                                    i += 3;
+                                }
+                                else
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += word[i + 2].ToString();
+                                    Syllable.Text += ",";
+                                    i += 2;
+                                }
+                            }
+                            else if ((word[i].Equals('A') || word[i].Equals('a')) && (word[i + 1].Equals('W') || word[i + 1].Equals('w')) && !isVowel(word, i + 1 + 1))
+                            {
+                                Syllable.Text += word[i + 1].ToString();
+                                Syllable.Text += word[i + 2].ToString();
+                                Syllable.Text += ",";
+                                i += 2;
+                            }
+
+                            else if ((word[i].Equals('N') || word[i].Equals('n')) && (word[i + 1].Equals('g') || word[i + 1].Equals('G')))
+                            {
+                                if (!isVowel(word, i + 1 + 1))
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += ",";
+
+                                }
+                                else
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+
+                                }
+                                i++;
+                            }
+                            else if ((word[i].Equals('C') || word[i].Equals('c')) && (word[i + 1].Equals('h') || word[i + 1].Equals('H')))
+                            {
+                                if (!isVowel(word, i + 1 + 1))
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += ",";
+
+                                }
+                                else
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+
+                                }
+                                i++;
+                            }
+                            else if (!isVowel(word, i) && !isVowel(word, i + 1))
+                            {
+                                Syllable.Text += ",";
+
+                            }
+                            else if ((isVowel(word, i) && isVowel(word, i + 1)) && (word[i] != word[i + 1]))
+                            {
+                                Syllable.Text += ",";
+
+                            }
+
+                            else if ((!isVowel(word, i) && isVowel(word, i + 1)) || (isVowel(word, i) && !isVowel(word, i + 1)))
+                            {
+                                if (word[i + 1 + 1] == 'g' || word[i + 1 + 1] == 'G')
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += word[i + 2].ToString();
+                                    Syllable.Text += ",";
+                                    i += 2;
+                                }
+                                else
+                                {
+                                    Syllable.Text += word[i + 1].ToString();
+                                    Syllable.Text += ",";
+                                    i++;
+                                }
+
+                            }
+                        }
+                        else
+                            continue;
+                    }
+                    Syllable.AppendText(Environment.NewLine);
+                }
+            });
+        }
+
+        private void MizoSyllabifier_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Are you sure to Exit?", "Exit?", MessageBoxButtons.YesNo);
+            if(res == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+           
+        }
+    }
+}
+
